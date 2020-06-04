aba200d10a4050707b0f27754cb5ae20df37d4a9
imanuddin.ardian@gmail.com <imanuddin.ardian@gmail.com>
2020-06-04 21:27:54 +0700

    Adding files.

---
+++ dist/precache-manifest.79d85e0ef2c95d3ee33b88e969760c49.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ff728759c523a1bfc9f",
+    "url": "/css/alert.bdc72f73.css"
+  },
+  {
+    "revision": "dece9c5e9ef34c623dfc",
+    "url": "/css/book.0de7565f.css"
+  },
+  {
+    "revision": "74a98cca0d49ee4934db",
+    "url": "/css/books.228bb759.css"
+  },
+  {
+    "revision": "03237144ede032302796",
+    "url": "/css/cart.07a87f9f.css"
+  },
+  {
+    "revision": "53d0abaa7cc3edef7a04",
+    "url": "/css/categories.228bb759.css"
+  },
+  {
+    "revision": "5853b194af99dc3711cc",
+    "url": "/css/category.11990f1e.css"
+  },
+  {
+    "revision": "3f1b80fb220d2778a390",
+    "url": "/css/checkout.1913d42e.css"
+  },
+  {
+    "revision": "b0fbf35aee496e846139",
+    "url": "/css/chunk-vendors.cc965617.css"
+  },
+  {
+    "revision": "6e69fe1f643afe675a36",
+    "url": "/css/my-order.5c8076a7.css"
+  },
+  {
+    "revision": "00ae15516d009dbdf1db",
+    "url": "/css/payment.57179a2d.css"
+  },
+  {
+    "revision": "2eda4d279796ffa24c56",
+    "url": "/css/profile.57179a2d.css"
+  },
+  {
+    "revision": "49bb2c7ead7a5f361823",
+    "url": "/css/register.5b5dffac.css"
+  },
+  {
+    "revision": "16ee7a39f562cfefe40e",
+    "url": "/css/search.d3f5352b.css"
+  },
+  {
+    "revision": "88b6672388a68dc84649688c8628625d",
+    "url": "/img/bca.png"
+  },
+  {
+    "revision": "8c9e358fea6803e97d766b8d1fdfabe2",
+    "url": "/img/fotoku.jpg"
+  },
+  {
+    "revision": "002a96bf1caef8c8142440b8a9348e5a",
+    "url": "/img/mandiri.png"
+  },
+  {
+    "revision": "74000e4216a44db09c5fe0bbd1785b48",
+    "url": "/img/unavailable.png"
+  },
+  {
+    "revision": "8c98ab83807cccb42c5f5de411716257",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72c472061b1698407745",
+    "url": "/js/about.af4be2fb.js"
+  },
+  {
+    "revision": "8ff728759c523a1bfc9f",
+    "url": "/js/alert.3aadebb0.js"
+  },
+  {
+    "revision": "fe42cf3d4f71ed1b6d60",
+    "url": "/js/app.06d2c03e.js"
+  },
+  {
+    "revision": "e62f8b70faf3944b0daf",
+    "url": "/js/book-item.c139c9dd.js"
+  },
+  {
+    "revision": "dece9c5e9ef34c623dfc",
+    "url": "/js/book.16e48a8d.js"
+  },
+  {
+    "revision": "74a98cca0d49ee4934db",
+    "url": "/js/books.9bb70030.js"
+  },
+  {
+    "revision": "03237144ede032302796",
+    "url": "/js/cart.fe542ba2.js"
+  },
+  {
+    "revision": "53d0abaa7cc3edef7a04",
+    "url": "/js/categories.730822f2.js"
+  },
+  {
+    "revision": "5853b194af99dc3711cc",
+    "url": "/js/category.eab2bfab.js"
+  },
+  {
+    "revision": "3f1b80fb220d2778a390",
+    "url": "/js/checkout.9189bbf5.js"
+  },
+  {
+    "revision": "b0fbf35aee496e846139",
+    "url": "/js/chunk-vendors.be6e5927.js"
+  },
+  {
+    "revision": "8dd10b84cfa6b44fe822",
+    "url": "/js/login.e5b8a702.js"
+  },
+  {
+    "revision": "6e69fe1f643afe675a36",
+    "url": "/js/my-order.c65e99ba.js"
+  },
+  {
+    "revision": "00ae15516d009dbdf1db",
+    "url": "/js/payment.902e966f.js"
+  },
+  {
+    "revision": "2eda4d279796ffa24c56",
+    "url": "/js/profile.74b0500d.js"
+  },
+  {
+    "revision": "49bb2c7ead7a5f361823",
+    "url": "/js/register.5b314c7a.js"
+  },
+  {
+    "revision": "16ee7a39f562cfefe40e",
+    "url": "/js/search.0743305e.js"
+  },
+  {
+    "revision": "641cb2b4aab443e906510eeb5fe0d36c",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
